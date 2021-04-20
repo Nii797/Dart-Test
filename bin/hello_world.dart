7a0757c321dcb7b989d0e9292ba39948214fca85
Nii797 <Phanterasonpardus797@gmail.com>
2021-04-21 00:10:13 +0700

    upload first dart

---
+++ bin/hello_world.dart
@@ -0,0 +1,28 @@
+// import 'package:hello_world/hello_world.dart' as hello_world;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${hello_world.calculate()}!');
+// }
+
+// void main(){
+//   for(int i=0; i<10; i++){
+//       print('Halo Soni selamat belajar dart dan fluter');
+//   }
+// }
+
+// variable type data
+void main(){
+  String name = 'Soni';
+  int age = 23;
+  double height = 1.80;
+  bool isMerried = false;
+
+  print('hello $name selamat datang di dart dan Flutter');
+  print('${name.length}');
+  
+  print('============= BIODATA DIRI =============');
+  print('Nama : $name');
+  print('Umur : $age');
+  print('Tinggi badan : $height Meter');
+  print('Status : $isMerried');
+}
